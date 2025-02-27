fa3a0590b75758987d6f8dfc1a121e68b53ec4bc
Gall-oDrone <piehavok@hotmail.com>
2025-02-26 20:00:22 -0600

    Code graded

--- evaluations/model_graded/custom_llm_eval.py
+++ evaluations/model_graded/custom_llm_eval.py
@@ -21,5 +21,12 @@ def llm_eval(summary, article):
         - Conciseness of 3: The summary captures most key points but could be more focused. It may include some
         - Conciseness of 5: The summary effectively condenses the main ideas into a brief, focused text. It
     2. Accuracy (1-5) - is the summary completely accurate based on the initial article?
-    3. Tone (1-5) - is the summary appropriate for a grade school student with no technical training?
+        - Accuracy of 1:
+        - Accuracy of 3:
+        - Accuracy of 5:
+    4. Tone (1-5) - is the summary appropriate for a grade school student with no technical training?
+        - Tone of 1:
+        - Tone of 3:
+        - Tone of 5:
+    5. Explanation - a general description of the way the summary is evaluated
     """
